2ec8709f30dac4ab78ab5cb88c95d3d6d5e71d97
devanshshukla99 <48386249+devanshshukla99@users.noreply.github.com>
2021-04-18 14:08:01 +0530

    improved

--- pytest_intercept/plugin.py
+++ pytest_intercept/plugin.py
@@ -3,10 +3,13 @@ import json
 import pytest
 from _pytest.monkeypatch import MonkeyPatch
 
+
 @pytest.hookimpl(tryfirst=True)
 def pytest_addoption(parser):
-    parser.addoption("--intercept", action="store_true", default=False, help="intercepts outgoing connections.")
-    # parser.addoption("--intercept-dump", action="store", default="url_records.json", help="file to dump intercepted urls.")
+    parser.addoption("--intercept", action="store", nargs="?", type=str,
+                     metavar='Intercept output file', default=None,
+                     help="intercepts outgoing connections and dumps it to a file."
+                     )
 
 
 def pytest_configure(config):
@@ -23,13 +26,14 @@ def monkeypatch_session():
     yield m
     m.undo()
 
+
 @pytest.fixture(autouse=True, scope="session")
 def no_http_requests(monkeypatch_session, pytestconfig):
     def requests_mock(self, method, url, *args, **kwargs):
         full_url = f"{self.scheme}://{self.host}{url}"
         pytest._requests_urls.append(full_url)
         raise RuntimeError(
-            f"3The test was about to {method} {full_url}"
+            f"The test was about to {method} {full_url}"
         )
 
     def socket_connect_mock(self, addr):
@@ -40,7 +44,7 @@ def no_http_requests(monkeypatch_session, pytestconfig):
 
         pytest._sockets_ip.append(f"{host}:{port}")
         raise RuntimeError(
-            f"2The test was about to connect to {host}:{port}"
+            f"The test was about to connect to {host}:{port}"
         )
 
     def urlopen_mock(self, http_class, req, **http_conn_args):
@@ -62,33 +66,15 @@ def no_http_requests(monkeypatch_session, pytestconfig):
 
 
 @pytest.fixture(scope='session', autouse=True)
-def write_intercept_teardown(pytestconfig):
+def intercept_teardown(pytestconfig):
     yield None
     if pytestconfig.getoption("intercept"):
+        intercept_file = pytestconfig.getoption("intercept")
         _urls = {'urllib': pytest._urllib_urls,
                  'requests': pytest._requests_urls,
                  'socket': pytest._sockets_ip,
                  }
-        with open("url_records.json", "w") as fd:
+        with open(intercept_file, "w") as fd:
             json.dump(_urls, fd)
             print("Dumped!")
     return
-
-
-# def pytest_unconfigure(monkeypatch_session):
-#     """
-#     Cleanup post-testing
-#     """
-#     monkeypatch_session.undo()
- 
-
-# def pytest_runtest_setup(item):
-#     print("Entering!")
-#     intercept = item.get_closest_marker('intercept')
-
-#     intercept_config = item.config.getvalue("intercept")
-#     if intercept is not None:
-#         pytest.skip("need --remote-data option to run")
-#     print("Exiting!")
-
-# # pytest.main(['--setup-show', './plugin.py'], plugins=[FixtureRegPlugin()])
