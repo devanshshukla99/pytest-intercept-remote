15458b897980f7e878f56d2e2dea7ca72b2e6157
devanshshukla99 <48386249+devanshshukla99@users.noreply.github.com>
2021-04-21 18:30:42 +0530

    plugin

--- pytest_intercept/plugin.py
+++ pytest_intercept/plugin.py
@@ -1,88 +1,71 @@
 import json
-
 import pytest
 from _pytest.monkeypatch import MonkeyPatch
 
 
-@pytest.hookimpl(tryfirst=True)
 def pytest_addoption(parser):
-    parser.addoption("--intercept", action="store", nargs="?", type=str,
-                     metavar='Intercept output file', default=None,
-                     help="intercepts outgoing connections and dumps it to a file."
-                     )
-    parser.addoption("--intercepted-file", action="store", nargs="?", type=str,
-                     metavar='Intercept output file', default=None,
-                     help="Supply intercepted file-name to any test function."
-                     )
+    _default_dump_file = "remote_urls.json"
+
+    parser.addoption(
+        "--intercept-remote", action="store_true", default=False,
+        help="Intercepts outgoing connections requests.")
+    parser.addoption(
+        "--intercept-dump-file", action="store", nargs="?", type=str,
+        metavar='dump filename', default=_default_dump_file,
+        help=f"filepath where the intercepted requests are dumped. (default: {_default_dump_file})")
 
 
 def pytest_configure(config):
+    pytest._intercept_remote = config.getoption('--intercept-remote')
+    pytest._intercept_dump_file = config.getoption('--intercept-dump-file')
+
     pytest._urllib_urls = []
     pytest._requests_urls = []
-    pytest._sockets_ip = []
-    config.getini('markers').append(
-        'intercept: Intercept')
-    config.getini('markers').append(
-        'intercepted_file: Supply intercepted file-name to any test function.')
-    pytest._intercepted_file = config.getoption('--intercepted-file')
+    pytest._sockets_hostport = []
 
-
-@pytest.fixture(scope='session')
-def monkeypatch_session():
-    m = MonkeyPatch()
-    yield m
-    m.undo()
+    if not pytest._intercept_remote and config.option.verbose:
+        print("Intercept outgoing requests disabled")
 
 
 @pytest.fixture(autouse=True, scope="session")
-def no_http_requests(monkeypatch_session, pytestconfig):
+def intercept_remote_requests():
+
     def requests_mock(self, method, url, *args, **kwargs):
         full_url = f"{self.scheme}://{self.host}{url}"
         pytest._requests_urls.append(full_url)
-        raise RuntimeError(
-            f"The test was about to {method} {full_url}"
-        )
+        pytest.xfail(f"The test was about to {method} {full_url}")
 
     def socket_connect_mock(self, addr):
-        if len(addr) == 2:  # AF_INET4
+        self.close()
+        if len(addr) == 2:
             host, port = addr
-        else:  # AF_INET6
+        else:
             host, port, _, _ = addr
-
-        pytest._sockets_ip.append(f"{host}:{port}")
-        raise RuntimeError(
-            f"The test was about to connect to {host}:{port}"
-        )
+        pytest._sockets_hostport.append(addr)
+        pytest.xfail(f"The test was about to connect to {host}:{port}")
 
     def urlopen_mock(self, http_class, req, **http_conn_args):
         pytest._urllib_urls.append(req.get_full_url())
-        raise RuntimeError(
-            f"The test was about to call {req.get_full_url()}"
-        )
+        pytest.xfail(f"The test was about to call {req.get_full_url()}")
 
-    if pytestconfig.getoption("intercept"):
+    if pytest._intercept_remote:
+        monkeypatch_session = MonkeyPatch()
         monkeypatch_session.setattr(
-            "urllib.request.AbstractHTTPHandler.do_open", urlopen_mock
-        )
+            "urllib.request.AbstractHTTPHandler.do_open", urlopen_mock)
         monkeypatch_session.setattr(
-            "urllib3.connectionpool.HTTPConnectionPool.urlopen", requests_mock
-        )
+            "urllib3.connectionpool.HTTPConnectionPool.urlopen", requests_mock)
         monkeypatch_session.setattr(
-            "socket.socket.connect", socket_connect_mock
-        )
+            "socket.socket.connect", socket_connect_mock)
 
-
-@pytest.fixture(scope='session', autouse=True)
-def intercept_teardown(pytestconfig):
     yield None
-    if pytestconfig.getoption("intercept"):
-        intercept_file = pytestconfig.getoption("intercept")
+
+    if pytest._intercept_remote:
         _urls = {
-            'urllib': pytest._urllib_urls,
-            'requests': pytest._requests_urls,
-            'socket': pytest._sockets_ip,
-            }
-        with open(intercept_file, "w") as fd:
+            'conn_urllib': pytest._urllib_urls,
+            'conn_requests': pytest._requests_urls,
+            'conn_socket': pytest._sockets_hostport}
+        with open(pytest._intercept_dump_file, 'w') as fd:
             json.dump(_urls, fd)
-            print("Dumped!")
+        monkeypatch_session.undo()
+
     return
