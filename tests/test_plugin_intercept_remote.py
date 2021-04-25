c05593b500913304cb6667f38ec008fe69a2cf0c
devanshshukla99 <48386249+devanshshukla99@users.noreply.github.com>
2021-04-25 11:30:57 +0530

    added remote tests

--- tests/test_plugin_intercept_remote.py
+++ tests/test_plugin_intercept_remote.py
@@ -18,6 +18,39 @@ def test_socket():
     assert s.connect(("www.python.org", 80)) is None
 
 
+def test_remote(testdir):
+    testdir.copy_example('pytest_intercept_remote/plugin.py')
+
+    # create a temporary pytest test file
+    testdir.makepyfile(
+        """
+        import pytest
+        from plugin import intercept_dump
+        from urllib.request import urlopen
+        import requests
+        import socket
+        def test_requests_urls():
+            u = requests.get("https://www.python.org")
+            assert u.status_code == 200
+
+        def test_urllib_urls():
+            u = urlopen("https://www.python.org/")
+            assert u.status == 200
+
+        def test_socket():
+            s = socket.socket()
+            assert s.connect(("www.python.org", 80)) is None
+
+        def test_dump():
+            intercept_dump()
+            assert open(pytest._intercept_dump_file).read() == '{"conn_urllib": [], "conn_requests": [], "conn_socket": []}'
+        """
+    )
+
+    result = testdir.runpytest("-vvv", "-s", "-raR")
+    result.assert_outcomes(passed=4)
+
+
 def test_intercept_remote(testdir):
     testdir.copy_example('pytest_intercept_remote/plugin.py')
 
@@ -47,5 +80,5 @@ def test_intercept_remote(testdir):
         """
     )
 
-    result = testdir.runpytest("--verbose", "--intercept-remote", "--intercept-dump-file=test_urls.json")
+    result = testdir.runpytest("-vvv", "-s", "-raR", "--intercept-remote", "--intercept-dump-file=test_urls.json")
     result.assert_outcomes(xfailed=3, passed=1)
